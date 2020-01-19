104d94dfab21f2d602c65cb05a9f7ee57bf51909
Jose Villasmil <jose.villasmil@doctum.cl>
2020-01-19 15:12:36 -0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('inspire')
+         $schedule->command('reportediario')
                   ->dailyAt('07:00');
     }
 
